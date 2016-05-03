91617c5c5287ac48fb17a9f979351da593f36abb
Alex <alex.lee3851@gmail.com>
2016-05-03 08:40:46 -0700

    added comments and spec fixes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,12 @@
 // YOUR CODE HERE:
-
-
 class App {
   constructor() {
     this.friends = {};
     this.server = 'https://api.parse.com/1/classes/messages';
     this.messages = [];
   }
+
+  // Initialize application
   init() {
     $('#send').unbind('submit').bind('submit', (e) => {
       e.preventDefault();
@@ -14,6 +14,8 @@ class App {
     });
     this.fetch();
   }
+
+  // Send message to server
   send(message) {
     $.ajax({
       url: this.server,
@@ -28,7 +30,9 @@ class App {
         console.error('chatterbox: Failed to send message', data);
       }
     });
-  }
+  } 
+
+  // Fetch messages from server
   fetch() {
     $.ajax({
       url: this.server,
@@ -43,38 +47,58 @@ class App {
       }
     });
   }
+
+  // Clear messages from chat window
   clearMessages() {
     $('#chats').html('');
   }
+
+  // Add a new message to chat window and send to server
   addMessage(message) {
-    var messageString = `<div><p><span class='username'>${message.username}</span>: ${message.text}</p></div>`;
+    var messageString = `<div><p><span class='username'>${filterXSS(message.username)}</span>: ${filterXSS(message.text)}</p></div>`;
     this.loadMessages(messageString);
     this.send(message);
   }
+
+  // Builds html elements to load fetched messages into chat window
   fetchMessages() {
     var newMessages = '';
     this.messages.forEach(message => {
-      newMessages = `<div><p><span class='username'>${message.username}</span>: ${message.text}</p></div>${newMessages}`;
+      if( message.username && message.text ){
+        newMessages = `<div><p><span class='username' data-username="${filterXSS(message.username)}">${filterXSS(message.username)}</span>: ${filterXSS(message.text)}</p></div>${newMessages}`;
+      }
     });
     this.loadMessages(newMessages);
   }
+
+  // Load messages onto the chat window
   loadMessages(messages) {
-    var $newMessages = $(filterXSS(messages));
-    // var $newMessages = $(messages);
+    var $newMessages = $(messages);
     $('#chats').append($newMessages);
-    $newMessages.find('.username').click( () => { this.addFriend(message.username); });
-    $('.chat-window').animate({scrollTop: $('.chat-window')[0].scrollHeight }, 1000);
+    $newMessages.find('.username').on( 'click', (event) => { console.log($(event.currentTarget).attr('data-username')) });
+    if( $('.chat-window').length > 0 ){ // Ensure chat-window exists before scrolling
+      $('.chat-window').animate({scrollTop: $('.chat-window')[0].scrollHeight }, 1000);
+    }
   }
+
+  // Add a new room
   addRoom(room) {
     var filteredRoom = filterXSS(room);
     $('#roomSelect').append(`<option value="${filteredRoom}">${filteredRoom}</option>`);
   }
+
+  // Add a friend
   addFriend(username) {
     this.friends[username] = username;
+    for( var friend in this.friends ){
+      console.log(this.friends[friend]);
+      $('.friends').append(this.friends[friend]);
+    }
   }
+
+  // Handle message submission
   handleSubmit() {
     var username = filterXSS(window.location.search.split('=')[1]);
-    // var message = filterXSS($('#message').val());
     var message = $('#message').val();
     var room = $('#roomSelect').val();
 
